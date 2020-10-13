bd0c4f0b1f39ff7e094f5853efb59da1bf65b9d7
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-13 22:58:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,4 +45,4 @@ def valid_move(board, index)
    end
  end
  
- if (position_taken?(board_index)) == false && (on_board?(index)) == true
+ if (position_taken?(board_index)) == false && (on_board?(index) == true)
