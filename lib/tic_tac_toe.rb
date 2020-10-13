37b39b997bf22cb1f7f72ea95804eeda0185fb56
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-13 23:24:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,7 @@ def turn(board)
   puts "Please choose a number 1 - 9"
   user_input = gets.chomp
   index = input_to_index(user_input)
-  if valid_move?
+  if valid_move?(board, index)
   
   
   
