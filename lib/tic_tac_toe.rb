5cba23dce3cbeac230171ab1118b32e5ddf75901
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-13 23:04:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ def display_board(board)
  puts "-----------"
  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-display_board(board)
+
 
 def input_to_index(user_input)
   user_input.to_i - 1
